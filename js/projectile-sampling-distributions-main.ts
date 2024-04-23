a627c03333816687c599f1103ad9f5b696701e88
matthew-blackman <mabl4992@colorado.edu>
2024-04-23 14:05:36 -0400

    Complete initial work to set up Sampling screen - see https://github.com/phetsims/projectile-sampling-distributions/issues/1

--- js/projectile-sampling-distributions-main.ts
+++ js/projectile-sampling-distributions-main.ts
@@ -3,15 +3,19 @@
 /**
  * Main entry point for the sim.
  *
- * @author matthew-blackman
+ * @author Matthew Blackman (PhET Interactive Simulations)
  */
 
-import Sim, { SimOptions } from '../../joist/js/Sim.js';
+import Sim from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
 import Tandem from '../../tandem/js/Tandem.js';
-import ProjectileSamplingDistributionsScreen from './projectile-sampling-distributions/ProjectileSamplingDistributionsScreen.js';
 import ProjectileSamplingDistributionsStrings from './ProjectileSamplingDistributionsStrings.js';
 import './common/ProjectileSamplingDistributionsQueryParameters.js';
+import SamplingScreen from '../../projectile-data-lab/js/sampling/SamplingScreen.js';
+import PreferencesModel from '../../joist/js/preferences/PreferencesModel.js';
+import SimulationPreferencesContentNode from '../../projectile-data-lab/js/common/view/SimulationPreferencesContentNode.js';
+import { Node } from '../../scenery/js/imports.js';
+import AudioPreferencesContentNode from '../../projectile-data-lab/js/common/view/AudioPreferencesContentNode.js';
 
 // Launch the sim. Beware that scenery Image nodes created outside simLauncher.launch() will have zero bounds
 // until the images are fully loaded. See https://github.com/phetsims/coulombs-law/issues/70#issuecomment-429037461
@@ -19,25 +23,43 @@ simLauncher.launch( () => {
 
   const titleStringProperty = ProjectileSamplingDistributionsStrings[ 'projectile-sampling-distributions' ].titleStringProperty;
 
+const preferencesModel = new PreferencesModel( {
+    simulationOptions: {
+      customPreferences: [ {
+        createContent: tandem => new SimulationPreferencesContentNode( tandem )
+      } ]
+    },
+    audioOptions: {
+      customPreferences: [
+        {
+
+          // Due to the layout considerations in the Preferences Dialog, it has 2 columns. Our entry for the left column
+          // is blank. See https://github.com/phetsims/joist/issues/956
+          createContent: () => new Node()
+        },
+        {
+          createContent: tandem => new AudioPreferencesContentNode( tandem )
+        } ]
+    }
+  } );
+
   const screens = [
-    new ProjectileSamplingDistributionsScreen( { tandem: Tandem.ROOT.createTandem( 'projectileSamplingDistributionsScreen' ) } )
+    new SamplingScreen( { tandem: Tandem.ROOT.createTandem( 'samplingScreen' ) } )
   ];
 
-  const options: SimOptions = {
-
-    //TODO fill in credits, all of these fields are optional, see joist.CreditsNode
+  const sim = new Sim( titleStringProperty, screens, {
     credits: {
-      leadDesign: '',
-      softwareDevelopment: '',
-      team: '',
-      contributors: '',
-      qualityAssurance: '',
-      graphicArts: '',
-      soundDesign: '',
-      thanks: ''
-    }
-  };
-
-  const sim = new Sim( titleStringProperty, screens, options );
+      leadDesign: 'Matthew Blackman',
+      softwareDevelopment: 'Matthew Blackman, Sam Reid',
+      team: 'Cathy Carter, Ariel Paul, Kathy Perkins, Amy Rouinfar',
+      contributors: 'Kelly Findley',
+      qualityAssurance: 'Nancy Salpepi, Kathryn Woessner',
+      graphicArts: 'Mariah Hermsmeyer',
+      soundDesign: 'Ashton Morris',
+      thanks: 'Bill Finzer, Gayle Geschwind, Heather Lewandowski, Joey Liu'
+    },
+    preferencesModel: preferencesModel,
+    phetioDesigned: true
+  } );
   sim.start();
 } );
