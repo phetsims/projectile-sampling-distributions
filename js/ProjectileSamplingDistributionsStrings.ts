47244274cd1eb34d786a72c68db6a37683f49650
Sam Reid <reids@colorado.edu>
2026-03-19 22:58:31 -0600

    Avoid namespace call, see https://github.com/phetsims/phet-core/issues/100

--- js/ProjectileSamplingDistributionsStrings.ts
+++ js/ProjectileSamplingDistributionsStrings.ts
@@ -19,6 +19,4 @@ type StringsType = {
 
 const ProjectileSamplingDistributionsStrings = getStringModule( 'PROJECTILE_SAMPLING_DISTRIBUTIONS' ) as StringsType;
 
-projectileSamplingDistributions.register( 'ProjectileSamplingDistributionsStrings', ProjectileSamplingDistributionsStrings );
-
 export default ProjectileSamplingDistributionsStrings;
--- js/common/ProjectileSamplingDistributionsQueryParameters.ts
+++ js/common/ProjectileSamplingDistributionsQueryParameters.ts
@@ -9,13 +9,10 @@
 
 import logGlobal from '../../../phet-core/js/logGlobal.js';
 import { QueryStringMachine } from '../../../query-string-machine/js/QueryStringMachineModule.js';
-import projectileSamplingDistributions from '../projectileSamplingDistributions.js';
 
 const ProjectileSamplingDistributionsQueryParameters = QueryStringMachine.getAll( {
   //TODO add schemas for query parameters, see https://github.com/phetsims/projectile-sampling-distributions/issues/2
 } );
-
-projectileSamplingDistributions.register( 'ProjectileSamplingDistributionsQueryParameters', ProjectileSamplingDistributionsQueryParameters );
 
 // Log query parameters
 logGlobal( 'phet.chipper.queryParameters' );
